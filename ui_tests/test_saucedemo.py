7a11081421064e566849a71a5cdd8a9f47ace10c
a-tishkova <sad057561@gmail.com>
2026-08-08 18:52:11 +0500

    Add negative login test with error validation

--- ui_tests/test_saucedemo.py
+++ ui_tests/test_saucedemo.py
@@ -4,3 +4,23 @@ def test_login_saucedemo(page):
     page.fill("#password", "secret_sauce")
     page.click("#login-button")
     assert page.locator(".title").text_content() == "Products"
+
+
+
+
+def test_failed_login_saucedemo(page):
+    # 1. Переходим на сайт
+    page.goto("https://saucedemo.com")
+    
+    # 2. Вводим правильный логин, но СЛУЧАЙНЫЙ НЕВЕРНЫЙ пароль
+    page.fill("#user-name", "standard_user")
+    page.fill("#password", "completely_wrong_password")
+    
+    # 3. Нажимаем кнопку входа
+    page.click("#login-button")
+    
+    # 4. Находим на странице красную плашку с ошибкой
+    error_message = page.locator("[data-test='error']").text_content()
+    
+    # 5. Проверяем (assert), что текст ошибки содержит информацию о несовпадении данных
+    assert "Username and password do not match any user in this service" in error_message
